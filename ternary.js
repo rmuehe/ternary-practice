8f57a0c2a5e677b2820b21685671165da6d0ba3f
rmuehe <40441350+rmuehe@users.noreply.github.com>
2019-08-01 11:22:13 -0700

    Add files via upload

---
+++ ternary.js
@@ -0,0 +1,183 @@
+var a = false;
+var b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, one, two, three, four, five, six;
+
+//This code goes 3 nesting levels
+// a 
+// ? b = true
+// ? d = true
+// ? h = true
+// : i = true
+// : e = true
+// ? j = true
+// : k = true
+
+// : c = true
+// ? f = true
+// ? l = true
+// : m = true
+// : g = true
+// ? n = true
+// : o = true
+
+
+//This code goes 4 nesting levels
+// a 
+// ? b = true
+// ? d = true
+// ? h = true 
+// ? p = true
+// : q = true
+// : i = true
+// ? r = true
+// : s = true
+// : e = true
+// ? j = true
+// ? t = true
+// : u = true
+// : k = true
+// ? v = true
+// : w = true
+
+// : c = true
+// ? f = true
+// ? l = true
+// ? x = true
+// : y = true
+// : m = true
+// ? z = true
+// : one = true
+// : g = true
+// ? n = true
+// ? two = true
+// : three = true
+// : o = true
+// ? four = true
+// : five = true
+
+
+// goes 5 nesting levels (with looping)
+
+a 
+? b = true
+? d = true
+? h = true 
+
+? p = true
+? a = true
+: b = true
+
+: q = true
+? c = true
+: d = true
+
+: i = true
+
+? r = true
+? e = true
+: f = true
+
+: s = true
+? g = true
+: h = true
+
+: e = true
+? j = true
+
+? t = true
+? i = true
+: j = true
+
+
+: u = true
+? k = true
+: l = true
+
+: k = true
+
+? v = true
+? m = true
+: n = true
+
+: w = true
+? o = true
+: p = true
+
+// Other half of the tree
+
+: c = true
+? f = true
+? l = true
+
+? x = true
+? q = true
+: r = true
+
+: y = true
+? s = true
+: t = true
+
+: m = true
+
+? z = true
+? u = true
+: v = true
+
+: one = true
+? w = true
+: x = true
+
+: g = true
+? n = true
+
+? two = true
+? y = true
+: z = true
+
+: three = true
+? one = true
+: two = true
+
+: o = true
+
+? four = true
+? three = true
+: four = true
+
+: five = true
+? five = true
+: six = true
+
+
+console.log("a is", a)
+console.log("b is", b)
+console.log("c is", c)
+console.log("d is", d)
+console.log("e is", e)
+console.log("f is", f)
+console.log("g is", g)
+console.log("h is", h)
+console.log("i is", i)
+console.log("j is", j)
+console.log("k is", k)
+console.log("l is", l)
+console.log("m is", m)
+console.log("n is", n)
+console.log("o is", o)
+console.log("p is", p)
+console.log("q is", q)
+console.log("r is", r)
+console.log("s is", s)
+console.log("t is", t)
+console.log("u is", u)
+console.log("v is", v)
+console.log("w is", w)
+console.log("x is", x)
+console.log("y is", y)
+console.log("z is", z)
+console.log("one is", one)
+console.log("two is", two)
+console.log("three is", three)
+console.log("four is", four)
+console.log("five is", five)
+console.log("six is", six)
+
